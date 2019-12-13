1cf0ab6d5b2659d94ab9eac9fc3b5a95ba235be1
WDcontrol <jerome.celebi@ynov.com>
2019-12-13 15:00:47 +0100

    user modify :tada:

--- models/user.js
+++ models/user.js
@@ -11,7 +11,11 @@ module.exports = {
   add() {
     //todo
   },
-  modify() {
-    //todo
-  },
+  modify(id, body) {
+    console.log(body);
+    return connection.execute(
+      'UPDATE client SET nom = ? , prenom = ? , adresse = ?, date_naissance=?, civilite=?, numero=?, id_ville=? WHERE id_client = ?',
+      [body.nom, body.prenom, body.adresse, body.date_naissance, body.civilite, body.numero, body.id_ville, id]
+    );
+  }
 }
